6712266a3aaad964c15fb4f1b8613870b1aa495f
Akshay Kadam <akshaykadam0904@gmail.com>
2018-08-10 19:32:10 +0530

    Add Menu for Copy/Paste fix for my first beta tester ✨

--- src/main/index.js
+++ src/main/index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, Menu, shell } from "electron";
 import * as path from "path";
 import { format as formatUrl } from "url";
 import { answerRenderer, callRenderer } from "electron-better-ipc";
@@ -60,6 +60,183 @@ const createMainWindow = () => {
   return window;
 };
 
+const createMenu = () => {
+  const template = [
+    {
+      label: "Edit",
+      submenu: [
+        {
+          role: "undo"
+        },
+        {
+          role: "redo"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "cut"
+        },
+        {
+          role: "copy"
+        },
+        {
+          role: "paste"
+        },
+        {
+          role: "pasteandmatchstyle"
+        },
+        {
+          role: "delete"
+        },
+        {
+          role: "selectall"
+        }
+      ]
+    },
+    {
+      label: "View",
+      submenu: [
+        {
+          label: "Reload",
+          accelerator: "CmdOrCtrl+R",
+          click(item, focusedWindow) {
+            if (focusedWindow) focusedWindow.reload();
+          }
+        },
+        {
+          label: "Toggle Developer Tools",
+          accelerator:
+            process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I",
+          click(item, focusedWindow) {
+            if (focusedWindow) focusedWindow.webContents.toggleDevTools();
+          }
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "resetzoom"
+        },
+        {
+          role: "zoomin"
+        },
+        {
+          role: "zoomout"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "togglefullscreen"
+        }
+      ]
+    },
+    {
+      role: "window",
+      submenu: [
+        {
+          role: "minimize"
+        },
+        {
+          role: "close"
+        }
+      ]
+    },
+    {
+      role: "help",
+      submenu: [
+        {
+          label: "Learn More",
+          click() {
+            shell.openExternal("http://electron.atom.io");
+          }
+        }
+      ]
+    }
+  ];
+
+  if (process.platform === "darwin") {
+    const name = app.getName();
+    template.unshift({
+      label: name,
+      submenu: [
+        {
+          role: "about"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "services",
+          submenu: []
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "hide"
+        },
+        {
+          role: "hideothers"
+        },
+        {
+          role: "unhide"
+        },
+        {
+          type: "separator"
+        },
+        {
+          role: "quit"
+        }
+      ]
+    });
+    // Edit menu.
+    template[1].submenu.push(
+      {
+        type: "separator"
+      },
+      {
+        label: "Speech",
+        submenu: [
+          {
+            role: "startspeaking"
+          },
+          {
+            role: "stopspeaking"
+          }
+        ]
+      }
+    );
+    // Window menu.
+    template[3].submenu = [
+      {
+        label: "Close",
+        accelerator: "CmdOrCtrl+W",
+        role: "close"
+      },
+      {
+        label: "Minimize",
+        accelerator: "CmdOrCtrl+M",
+        role: "minimize"
+      },
+      {
+        label: "Zoom",
+        role: "zoom"
+      },
+      {
+        type: "separator"
+      },
+      {
+        label: "Bring All to Front",
+        role: "front"
+      }
+    ];
+  }
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+};
+
 // quit application when all windows are closed
 app.on("window-all-closed", () => {
   // on macOS it is common for applications to stay open until the user explicitly quits
@@ -78,6 +255,7 @@ app.on("activate", () => {
 // create main BrowserWindow when electron is ready
 app.on("ready", () => {
   mainWindow = createMainWindow();
+  createMenu();
 });
 
 answerRenderer("app-quit", () => {
